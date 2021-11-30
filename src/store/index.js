c7b2d349cb53c1ebe2e6984c69fd3b4b6f6ed4fe
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-11-29 20:20:20 -0400

    ➕ WIP: add pinia

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createPinia } from 'pinia';
+
+export default createPinia({
+    state: {
+        user: null
+    }
+});
---
+++ src/store/useAuth.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useAuth = defineStore('auth', {
+    state: () => ({
+        user: null
+    }),
+    getters: {
+
+    },
+    actions: {
+
+    }
+})
