251be165a72723b523787cd32fe4f02e44c89305
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-05-15 08:52:22 -0400

    chore: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
   "core": {
-    "builder": "storybook-builder-vite"
+    "builder": "@storybook/builder-vite"
   },
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
     config.module.rules.push({
       test: /\.css$/,
-      use: [
-        'vue-style-loader', 'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [
-              require('tailwindcss'),
-              require('autoprefixer'),
-            ],
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../src/'),
-    })
-
+      use: ['vue-style-loader', 'css-loader', {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: [require('tailwindcss'), require('autoprefixer')]
+        }
+      }],
+      include: path.resolve(__dirname, '../src/')
+    });
     return config;
-  },
-}
+  }
+};
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,20 +1,20 @@
-import MyHeader from '../components/Header.vue';
+import AppHeader from '../components/Header.vue';
 
 export default {
   title: 'Components/Header',
-  component: MyHeader,
+  component: AppHeader,
 };
 
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { MyHeader },
+  components: { AppHeader },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     // Story args can be spread into the returned object
     return { ...args };
   },
   // Then, the spread values can be accessed directly in the template
-  template: '<my-header :user="user" />',
+  template: '<AppHeader :user="user" />',
 });
 
 export const LoggedIn = Template.bind({});
