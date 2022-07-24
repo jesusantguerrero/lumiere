2a6b27083355530976bfd13d5d11f9166f2eef46
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-07-24 12:45:57 -0400

    chore: update dependencies

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,30 +1,28 @@
-import AppHeader from '../components/Header.vue';
+import MyHeader from './Header.vue';
 
 export default {
-  title: 'Components/Header',
-  component: AppHeader,
+  title: 'Example/Header',
+  component: MyHeader,
 };
 
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { AppHeader },
+  components: { MyHeader },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     // Story args can be spread into the returned object
     return { ...args };
   },
   // Then, the spread values can be accessed directly in the template
-  template: '<AppHeader :user="user" />',
+  template: '<my-header :user="user" />',
 });
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  title: "Lumiere",
   user: {},
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
-  title: "Lumiere",
   user: null,
 };
